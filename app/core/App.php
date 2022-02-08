43dafb229bf86588fc4e79b8284997d5cf553b14
hrvojefuri <93526792+hrvojefuri@users.noreply.github.com>
2022-02-08 19:30:21 +0100

    Dohvaćanje rute korisnika

--- app/core/App.php
+++ app/core/App.php
@@ -4,6 +4,10 @@ class App
 {
     public static function start()
     {
-        echo 'Hello World!';
+        // echo '<pre>';
+        // print_r($_SERVER);
+        // echo '</pre>';
+        $ruta = Request::getRuta();
+        echo $ruta;
     }
 }

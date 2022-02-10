13bac5b93d3013462985b16643aafefc08ca99fb
hrvojefuri <93526792+hrvojefuri@users.noreply.github.com>
2022-02-10 23:26:03 +0100

    Uspostava View komponente

---
+++ app/core/View.php
@@ -0,0 +1,20 @@
+<?php
+
+class View
+{
+    private $predlozak;
+
+    public function __construct($predlozak = 'predlozak')
+    {
+        $this->predlozak=$predlozak;
+    }
+
+    public function render($phtmlStranica,$parametri=[])
+    {
+        ob_start();
+        extract($parametri); // od asocijativnog niza kreira varijable
+        include_once BP_APP . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR . $phtmlStranica . '.phtml';
+        $sadrzaj = ob_get_clean();
+        include_once BP_APP . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR . $this->predlozak . '.phtml';
+    }
+}

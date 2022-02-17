bda51496448e83cac1123c9804826d57f5fc25cc
hrvojefuri <93526792+hrvojefuri@users.noreply.github.com>
2022-02-17 20:07:51 +0100

    Priprema spajanja na bazu

---
+++ app/core/DB.php
@@ -0,0 +1,15 @@
+<?php
+
+class DB extends PDO
+{
+    private static $instanca = null;
+
+    public static function getInstanca()
+    {
+        if(self::$instanca==null){
+            self::$instanca=new self();        
+        }
+        return self::$instanca;
+    }
+
+}

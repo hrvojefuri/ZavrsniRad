13589e821b8cf136cb1d8f73942b3d9131ac741a
hrvojefuri <93526792+hrvojefuri@users.noreply.github.com>
2022-02-25 01:31:49 +0100

    Uređivanje view-a proizvod

--- app/model/Proizvod.php
+++ app/model/Proizvod.php
@@ -10,7 +10,11 @@ class Proizvod
         $veza = DB::getInstanca();
         $izraz = $veza->prepare('
         
-            select * from proizvod;
+        select a.sifra, a.zanr, a.izvodac, a.naziv, a.cijena, a.izdavackaKuca, a.zaliha,
+        count(b.sifra) as kosarica
+        from proizvod a left join kosarica b
+        on a.sifra=b.proizvod
+        group by a.sifra, a.zanr, a.izvodac, a.naziv, a.cijena, a.izdavackaKuca, a.zaliha;
         
         ');
         $izraz->execute();

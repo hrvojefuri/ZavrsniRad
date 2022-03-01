4de7ef48b8138d27a89ba440dba6d25acb25b29a
hrvojefuri <93526792+hrvojefuri@users.noreply.github.com>
2022-03-01 19:21:52 +0100

    Formatiranje broja

--- app/controller/ProizvodController.php
+++ app/controller/ProizvodController.php
@@ -5,11 +5,25 @@ class ProizvodController extends AutorizacijaController
 
     private $viewDir = 'privatno' . DIRECTORY_SEPARATOR . 'proizvodi' . DIRECTORY_SEPARATOR;
 
+    private $nf;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->nf=new \NumberFormatter("hr-HR", \NumberFormatter::DECIMAL);
+        $this->nf->setPattern('#,##0.00 kn');
+    }
+
     public function index()
     {
-        // print_r(Proizvod::read());
+        $proizvodi = Proizvod::read();
+
+        foreach($proizvodi as $proizvod){
+            $proizvod->cijena=$this->nf->format($proizvod->cijena);            
+        }
+        
         $this->view->render($this->viewDir . 'index',[
-            'proizvodi'=>Proizvod::read(),
+            'proizvodi'=>$proizvodi,
             'css'=>'<link rel="stylesheet" href="' . App::config('url') . 'public/css/proizvodindex.css">'
         ]);
     }
